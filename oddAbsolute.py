10be33f45d79db80830a4f669087e34ed92f97f0
Larry Winton <Larry.Winton@vu.com>
2022-02-16 10:37:43 -0600

    4.5.1 try 1

--- oddAbsolute.py
+++ oddAbsolute.py
@@ -2,6 +2,24 @@ def calculateAbsolute():
     
     # This first line is provided for you
     in_num  = input("Enter a number: ")
+    iTarget = 21
+    # print("target :", iTarget)
+    iNumber = int(in_num)
+    # print("iNumber raw :", iNumber)
+    bBigNumber = iNumber > iTarget
+    # if bBigNumber:
+    #     print("iNumber is greather than 21")
+    # else:
+    #     print("iNumber is less than 21")
+    absNumber = abs(iNumber)
+    # print("absolute iNumber :", absNumber)
+    test = iTarget - iNumber
+    # print("test :", test)
+    absTest = abs(test)
+    # print("absTest :", absTest)
+    if bBigNumber:
+        absTest += absTest
+    print("Result:", absTest)
     # end assignment
 
 ## if you want to test locally before you try to sync
